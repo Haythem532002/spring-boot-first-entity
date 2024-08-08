3d434d753394f5a93504f22606199fe5835e34e6
Haythem532002 <khiarih06@gmail.com>
2024-08-08 16:16:11 +0100

    add starter files

---
+++ src/main/java/com/first_entity/firstEntity/FirstEntityApplication.java
@@ -0,0 +1,13 @@
+package com.first_entity.firstEntity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstEntityApplication.class, args);
+	}
+
+}
