df07f3fb0c179f3661bac397f39e622002d7e771
Haythem532002 <khiarih06@gmail.com>
2024-08-11 15:38:27 +0100

    add swagger to document the rest api

---
+++ src/main/java/com/first_entity/firstEntity/config/OpenApiConfig.java
@@ -0,0 +1,40 @@
+package com.first_entity.firstEntity.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+import javax.naming.Name;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Haythem",
+                        email = "khiarih06@gmail.com",
+                        url = "https://haythemSchool.com"
+                ),
+                description = "OpenApi documentation for Spring Boot Application",
+                title = "OpenApi specification - Haythem",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://haythemSchool.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8090/api/v1"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://haythemSchool.com"
+                )
+        }
+)
+public class OpenApiConfig {
+}
